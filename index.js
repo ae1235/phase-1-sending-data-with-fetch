af82b5179f4ab1802510ab51672226100b7928ce
+ <+>
2022-06-17 16:15:20 +0300

    commit tree 3abc30d6b7e273a0750e23340696f807ba43b64f

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your code here
+
+const body = document.querySelector('body')
+
+function submitData(userName, userEmail) {
+    const baseUrl = 'http://localhost:3000/users'
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    }
+
+    return fetch(baseUrl, configurationObject)
+    .then(res => res.json())
+    .then(newUser => renderDataToDOM(newUser))
+    .catch(error => {
+        renderErrToDOM(error)
+    })
+}
+
+function renderDataToDOM(newUser) {
+    body.append(newUser.id)
+}
+
+function renderErrToDOM(error) {
+    body.append(error)
+}
+
+submitData('steve', 'steve@steve.com')
